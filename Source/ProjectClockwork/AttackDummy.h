06fad1b5a2c4a61e81d3235300d0dc8ba7a06d7a
LudvigRisan <ludvig.risan@gmail.com>
2017-03-14 15:34:16 +0100

    Attackdummy
    Began work on attackdummy, did some cleanup

---
+++ Source/ProjectClockwork/AttackDummy.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "DamageTarget.h"
+#include "AttackDummy.generated.h"
+
+UCLASS()
+class PROJECTCLOCKWORK_API AAttackDummy : public ADamageTarget
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAttackDummy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
