f0eb3aeba890852c62d6a4f34ea0eaee81106ea0
LudvigRisan <ludvig.risan@gmail.com>
2017-04-25 19:25:26 +0200

    Loads!
    Lots of stuff changed and added.

--- Source/ProjectClockwork/AP1FurnaceVomit.cpp
+++ Source/ProjectClockwork/AP1FurnaceVomit.cpp
@@ -28,14 +28,14 @@ void AAP1FurnaceVomit::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	if (ready) {
-		shootingTimer += DeltaTime;
+		shootingTimer += DeltaTime;															//Shoot multiple bullets per frame depending on firerate
 		for (shootingTimer; shootingTimer >= 1 / fireRate; shootingTimer -= 1/fireRate) {
 			AAP1FurnaceVomit::aim(DeltaTime);
 			AAP1FurnaceVomit::shoot(direction);
 		}
 	}
 
-	age += DeltaTime;
+	age += DeltaTime;																		//SelfdestructTimer
 	if (age >= lifeTime) {
 		AAP1FurnaceVomit::endAttack();
 	}
@@ -49,32 +49,34 @@ void AAP1FurnaceVomit::beginAttack() {
 
 	FVector loc = {boss->GetActorLocation().X, boss->GetActorLocation().Y, 150 };
 
-	SetActorLocation(loc);
+	SetActorLocation(loc);																	//move to the boss
 
-	wave = waveOrigin;
+	wave = waveOrigin;																		//set the wave direction to its starting position
 
 	ready = true;
 }
 
-void AAP1FurnaceVomit::endAttack() {
+void AAP1FurnaceVomit::endAttack() {														//get the boss to start its next attack
 	boss->chooseAttack();
 	Destroy();
 }
 
 void AAP1FurnaceVomit::aim(float DeltaTime) {
-	if (waveDir) {
+	if (waveDir) {																			//Decide the direction it will rotate
 		wave += waveSpeed;
-		if (wave >= waveOrigin + waveWidth) {
+		if (wave >= waveOrigin + waveWidth) {												//Reverse direction of rotation
+			wave = (waveOrigin + waveWidth) - (wave - (waveOrigin + waveWidth));
 			waveDir = false;
 		}
 	} else {
 		wave -= waveSpeed;
 		if (wave <= waveOrigin - waveWidth) {
+			wave = (waveOrigin - waveWidth) - (wave - (waveOrigin - waveWidth));
 			waveDir = true;
 		}
 	}
-	direction = {sinf(wave), cosf(wave), 0};
-	offset = direction * shootingTimer * projectileSpeed;
+	direction = {sinf(wave), cosf(wave), 0};												//Use the rotation to calculate a movement direction
+	offset = direction * shootingTimer * projectileSpeed;									//Calculate an offset to create a smooth line of bullets
 }
 
 void AAP1FurnaceVomit::shoot(FVector dir) {
@@ -82,7 +84,7 @@ void AAP1FurnaceVomit::shoot(FVector dir) {
 	if (world) {
 		ABullet* bullet = world->SpawnActor<ABullet>(BulletBlueprint, GetActorLocation() + offset, dir.Rotation());
 		if (bullet) {
-			bullet->launch(dir * projectileSpeed);
+			bullet->launch(dir * projectileSpeed);											//Set the bullets movement
 		}
 	}
 }
